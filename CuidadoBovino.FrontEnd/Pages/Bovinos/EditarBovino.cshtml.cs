63ab7540935e38461feb19068d33e1bb24d38799
carlos-84 <carlos.barriosv@outlook.es>
2022-09-09 22:56:02 -0500

    Añadiendo la pagina de bovinos, agregar, editar y eliminar bovinos tambien se agrego fondo de pagina

---
+++ CuidadoBovino.FrontEnd/Pages/Bovinos/EditarBovino.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CuidadoBovino.FrontEnd.Pages
+{
+    public class EditarBovinoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ CuidadoBovino.FrontEnd/Pages/Bovinos/EliminarBovino.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CuidadoBovino.FrontEnd.Pages
+{
+    public class EliminarBovinoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
