cd661197226bea682ed1fe1f5911f9747d7d5e95
Davidfn11 <davidf1_1910@hotmail.com>
2022-09-14 20:25:42 -0500

    Mostrar detalles de los bovinos

--- CuidadoBovino.FrontEnd/Pages/Bovinos/Detalles.cshtml.cs
+++ CuidadoBovino.FrontEnd/Pages/Bovinos/Detalles.cshtml.cs
@@ -1,12 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using CuidadoBovino.App.Dominio;
+using CuidadoBovino.App.Persistencia;
+using Microsoft.AspNetCore.Authorization;
 
 namespace CuidadoBovino.FrontEnd.Pages
 {
     public class DetallesModel : PageModel
     {
-        public void OnGet()
+        private readonly IntBovino repositorioB;
+        public Bovino bovino {set; get;}
+        public DetallesModel(){
+            this.repositorioB = new RepBovino(new CuidadoBovino.App.Persistencia.AppContext());
+        }
+        public IActionResult OnGet(int IdBovino)
         {
+            bovino = repositorioB.GetBovino(IdBovino);
+            if (bovino==null)
+            {
+                return RedirectToPage("./NoFound");
+            }
+            else{
+               return Page();
+            }
         }
     }
 }
