b432ab79e88f7e58b59d3e419b449b8b6fc8efb5
carlos-84 <carlos.barriosv@outlook.es>
2022-09-20 18:12:56 -0500

    Completando el crud de bovino, con la pagina de eliminar y agregar

--- CuidadoBovino.FrontEnd/Pages/Bovinos/AgregarBovino.cshtml.cs
+++ CuidadoBovino.FrontEnd/Pages/Bovinos/AgregarBovino.cshtml.cs
@@ -1,12 +1,32 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using CuidadoBovino.App.Dominio;
+using CuidadoBovino.App.Persistencia;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
 
 namespace CuidadoBovino.FrontEnd.Pages
 {
     public class AgregarBovinoModel : PageModel
     {
+        private readonly IntBovino repBovino;
+
+        public AgregarBovinoModel()
+        {
+            this.repBovino = new RepBovino(new CuidadoBovino.App.Persistencia.AppContext());
+        }
+        [BindProperty]
+        public Bovino bovino{set;get;}
         public void OnGet()
         {
+        }
+        public IActionResult OnPost()
+        {
+            repBovino.AddBovino(bovino);
+            return RedirectToPage("./Bovinos");
         }
     }
 }
