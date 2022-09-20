f2b80311c424ad1aeab3e1b7676a55213bcc1a33
Davidfn11 <davidf1_1910@hotmail.com>
2022-09-20 12:05:35 -0500

    Agrega Bovino

--- CuidadoBovino.FrontEnd/Pages/Bovinos/AgregarBovino.cshtml.cs
+++ CuidadoBovino.FrontEnd/Pages/Bovinos/AgregarBovino.cshtml.cs
@@ -1,12 +1,33 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CuidadoBovino.App.Dominio;
+using CuidadoBovino.App.Persistencia;
 
 namespace CuidadoBovino.FrontEnd.Pages
 {
     public class AgregarBovinoModel : PageModel
     {
+        private readonly IntBovino RBovino;
+
+        public AgregarBovinoModel()
+        {
+         this.RBovino = new RepBovino(new CuidadoBovino.App.Persistencia.AppContext());
+        }
+        [BindProperty]
+        public Bovino Bovino{set;get;}
+        public Veterinario Veterinario{set; get;}
+        public HistoriaMedica HistoriaMedica{set;get;}
         public void OnGet()
         {
+        }
+        public IActionResult OnPost()
+        {
+          RBovino.AddBovino(Bovino);
+          return RedirectToPage("./Bovinos");
         }
     }
 }
