b402b69e9be6b6054c783757c62e26e1caded5bb
Davidfn11 <davidf1_1910@hotmail.com>
2022-09-22 11:49:38 -0500

    1ra parte DueñoBovino Adicion dueño

--- CuidadoBovino.FrontEnd/Pages/Bovinos/AgregarBovino.cshtml.cs
+++ CuidadoBovino.FrontEnd/Pages/Bovinos/AgregarBovino.cshtml.cs
@@ -6,48 +6,31 @@ using System.Linq;
 using System.Threading.Tasks;
 using CuidadoBovino.App.Dominio;
 using CuidadoBovino.App.Persistencia;
-using CuidadoBovino.App.Dominio;
-using CuidadoBovino.App.Persistencia;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 
 namespace CuidadoBovino.FrontEnd.Pages
 {
     public class AgregarBovinoModel : PageModel
     {
-        private readonly IntBovino RBovino;
-
-        public AgregarBovinoModel()
-        {
-         this.RBovino = new RepBovino(new CuidadoBovino.App.Persistencia.AppContext());
-        }
-        [BindProperty]
-        public Bovino Bovino{set;get;}
-        public Veterinario Veterinario{set; get;}
-        public HistoriaMedica HistoriaMedica{set;get;}
         private readonly IntBovino repBovino;
 
         public AgregarBovinoModel()
         {
-            this.repBovino = new RepBovino(new CuidadoBovino.App.Persistencia.AppContext());
+         this.repBovino = new RepBovino(new CuidadoBovino.App.Persistencia.AppContext());
         }
         [BindProperty]
         public Bovino bovino{set;get;}
+        //public Veterinario Veterinario{set; get;}
+        //public HistoriaMedica HistoriaMedica{set;get;}
+       
         public void OnGet()
         {
         }
         public IActionResult OnPost()
         {
-          RBovino.AddBovino(Bovino);
+          repBovino.AddBovino(bovino);
           return RedirectToPage("./Bovinos");
         }
-        public IActionResult OnPost()
-        {
-            repBovino.AddBovino(bovino);
-            return RedirectToPage("./Bovinos");
-        }
+        
     }
 }
